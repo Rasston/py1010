0bc2169e77a4f3bbf24ca56916dc2c3707c266a7
Rasston <sandersundt@outlook.com>
2024-11-02 00:03:56 +0100

    Add files via upload

---
+++ arbeidskrav_1_srs_v01.py
@@ -0,0 +1,49 @@
+"""
+Created on Sat Oct 26 15:47:31 2024
+
+@author: Sander Ryen Sundt, epost: sandersundt@outlook.com
+
+Arbeidskrav 1 PY 1010
+
+Sist oppdatert 26.10.24
+"""
+# %% Faste verdier for begge kjoeretoey
+
+K = 10000  # Kjoerelengde
+TAD = 8.38  # Trafikkforsikringsavgift pr dag
+TAA = TAD*365  # Trafikkforsikringsavgift pr år
+
+# %% Utgifter elbil
+
+FE = 5000  # Aerlig utgift forsikring elbil
+SF = 0.2  # Stroemforbruk i kWh pr km elbil
+SP = 2.00  # Stroempris pr kWh
+DUE = SF*SP*K  # Aerlig utgift drivstoff elbil
+BE = 0.1  # Utgift bomavgift pr km elbil
+SUE = FE + DUE + BE*K + TAA  # Samlede utgifter pr aer elbil
+
+# %% Utgifter bensinbil
+
+FB = 7500  # Aerlig utgift forsikring bensinbil
+DB = 1.0  # Utgift drivstoff pr km bensinbil
+DUB = DB*K  # Aerlig utgift drivstoff bensinbil
+BB = 0.3  # Utgift bomavgift pr km bensinbil
+SUB = FB + DUB + BB*K + TAA  # Samlede utgifter pr aer bensinbil
+
+# %% Utskrift og sammenligning
+
+print("Samlede utgifter pr aer bensinbil:", SUB)
+print("Samlede utgifter pr aer elbil:", SUE)
+print("Forskjell samlede utgifter:", SUB - SUE)
+
+print("Utgifter drivstoff pr aer bensinbil:", DUB)
+print("Utgifter drivstoff pr aer elbil:", DUE)
+print("Forskjell drivstoffutgifter:", DUB - DUE)
+
+print("Utgifter bomavgift pr aer bensinbil:", BB*K)
+print("Utgifter bomavgift pr aer elbil:", BE*K)
+print("Forskjell utgifter bomavgift:", BB*K - BE*K)
+
+print("Utgift forsikring bensinbil:", FB)
+print("Utgift forsikring elbil:", FE)
+print("Forskjell utgift forsikring:", FB - FE)
